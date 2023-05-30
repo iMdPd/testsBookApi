420e167dbe03011434e890763dc2cf6c7838e68c
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-30 20:04:32 +0200

    Create content.length test

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -9,4 +9,8 @@ describe("cutText", () => {
     expect(cutText([], 20)).to.equal("Error");
     expect(cutText(function () {}, 20)).to.equal("Error");
   });
+
+  it('should return an error if "content" arg length is 0', () => {
+    expect(cutText("", 20)).to.equal("Error");
+  });
 });
