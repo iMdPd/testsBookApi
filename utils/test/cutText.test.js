f86a43375fdd0adf608120c2f83e4cdf6c340533
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-30 20:03:12 +0200

    Add content typeof test

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,12 @@
+cutText = require("../cutText");
+const expect = require("chai").expect;
+
+describe("cutText", () => {
+  it('should return an error if "content" arg is not a string', () => {
+    expect(cutText(undefined, 20)).to.equal("Error");
+    expect(cutText(12, 20)).to.equal("Error");
+    expect(cutText({}, 20)).to.equal("Error");
+    expect(cutText([], 20)).to.equal("Error");
+    expect(cutText(function () {}, 20)).to.equal("Error");
+  });
+});
