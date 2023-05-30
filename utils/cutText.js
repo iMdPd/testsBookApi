6db061075725d7d3d9a5bd7629f11afbfa72708e
Mariusz Politowicz <politowiczmar@gmail.com>
2023-05-30 20:07:54 +0200

    Create maxLength arg typeof test

--- utils/cutText.js
+++ utils/cutText.js
@@ -1,6 +1,10 @@
 module.exports = (content, maxLength) => {
-  if (typeof content !== "string") return "Error";
-  if (content.length < 1) return "Error";
+  if (
+    typeof content !== "string" ||
+    content.length < 1 ||
+    typeof maxLength !== "number"
+  )
+    return "Error";
   if (content.length <= maxLength) return content;
-  return content.substr(0, content.lastIndexOf(" ", maxLength)) + "...";
+  return content.substring(0, content.lastIndexOf(" ", maxLength)) + "...";
 };
